f30b5ec05897a70c337e9748e670c6a281c99b62
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-04 00:04:09 +0300

    Foundational codes initalization
    I'm committing them seperately for getting them back if something wents wrong

---
+++ Source/Elementalist/ElementalistGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ElementalistGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AElementalistGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AElementalistGameMode();
+};
+
+
+
